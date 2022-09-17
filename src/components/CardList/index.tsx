73e446adb887e0cdcb013e469ccbc1f6c2405d34
Julia_Vladomira <juliavladomira@gmail.com>
2022-09-17 13:21:47 +0200

    adds header and list section

---
+++ src/components/CardList/index.tsx
@@ -0,0 +1,9 @@
+export const CardsList = () => {
+    return (
+        <div className='section list-section'>
+            <div className='container'>
+                <h1 className='title'>Choose a meeting room</h1>
+            </div>
+        </div>
+    );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <div className='section header'>
+            <div className='container'>
+                <img alt='logo' src='images/svg/logo.svg' className='image' />
+            </div>
+        </div>
+    );
+};
