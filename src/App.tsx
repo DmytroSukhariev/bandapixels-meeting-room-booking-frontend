a99873eae15550d0a4d89d60c3908ec3df6c441c
Serghey Samokhval <samsonskiff@gmail.com>
2022-09-18 16:13:26 +0300

    add info modal

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,31 @@ function App() {
   console.log({ state });
   return (
     <div className='App'>
-      <style>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
-          rel='stylesheet'
-        />
-      </style>
-      <MainPage />
-      <MyCalendar />
-      <Modal isOpen={state.bookingModal} toggleModal={setBookingModal}>
-        <BookingModal toggleModal={setBookingModal} />
-      </Modal>
-        {/*<Modal isOpen={state.aboutBookedModal} toggleModal={setAboutBookedModal}>*/}
-        {/*    <InfoModal*/}
-        {/*        toggleModal={setAboutBookedModal}*/}
-        {/*        bookedRoomInfo={{*/}
-        {/*            issuerFirstName: 'Me',*/}
-        {/*            issuerLastName: 'MeMe',*/}
-        {/*            meetingRoomId: 2,*/}
-        {/*            description: 'gdgdfhdfh',*/}
-        {/*            numberOfPeople: 2,*/}
-        {/*            start: new Date('11.02.13'),*/}
-        {/*            end: new Date('11.02.13'),*/}
-        {/*        }}*/}
-        {/*    />*/}
-        {/*</Modal>*/}
+        <style>
+            <link rel='preconnect' href='https://fonts.googleapis.com'/>
+            <link rel='preconnect' href='https://fonts.gstatic.com'/>
+            <link
+                href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+                rel='stylesheet'
+            />
+        </style>
+        <MainPage/>
+        <MyCalendar/>
+        <Modal isOpen={state.bookingModal || state.aboutBookedModal} toggleModal={setBookingModal}>
+            {state.bookingModal && <BookingModal toggleModal={setBookingModal}/>}
+            {state.aboutBookedModal && <InfoModal
+                toggleModal={setAboutBookedModal}
+                bookedRoomInfo={{
+                    issuerFirstName: 'Me',
+                    issuerLastName: 'MeMe',
+                    meetingRoomId: 2,
+                    description: 'gdgdfhdfh',
+                    numberOfPeople: 2,
+                    start: new Date('11.02.13'),
+                    end: new Date('11.02.13'),
+                }}
+            />}
+        </Modal>
     </div>
   );
 }
